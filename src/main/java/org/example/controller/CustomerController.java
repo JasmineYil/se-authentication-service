9296fa1a57af109c341da255d50038f399f54421
JasmineYil <71649452+JasmineYil@users.noreply.github.com>
2024-04-29 22:41:09 +0200

    All Files for Authentication and Customer added. MongoDB Connection added. README.md added.

---
+++ src/main/java/org/example/controller/CustomerController.java
@@ -0,0 +1,94 @@
+package org.example.controller;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.dto.CustomerDto;
+import org.example.dto.CustomerLoginDto;
+import org.example.dto.LoginResponseDto;
+import org.example.exception.EmailAlreadyInUseException;
+import org.example.model.Customer;
+import org.example.service.CustomerService;
+import org.example.service.JwtService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+//@CrossOrigin(origins = "http://localhost:9090") // The URL of frontend
+public class CustomerController {
+
+    private final JwtService jwtService;
+    private final CustomerService customerService;
+    private final HttpServletResponse httpServletResponse;
+
+    public CustomerController(JwtService jwtService, CustomerService customerService, HttpServletResponse httpServletResponse) {
+        this.jwtService = jwtService;
+        this.customerService = customerService;
+        this.httpServletResponse = httpServletResponse;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerCustomer(@RequestBody CustomerDto customerDto) {
+        try {
+            customerService.registerCustomer(customerDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Customer registered successfully");
+        } catch (EmailAlreadyInUseException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("The information provided cannot be used to create a new account. Please double-check your details or try different ones.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Customer registration failed");
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginCustomer(@RequestBody CustomerLoginDto loginCustomerDto) {
+        try {
+            Customer loginCustomer = customerService.authenticateCustomer(loginCustomerDto);
+            String jwtToken = jwtService.generateToken(loginCustomer);
+
+            LoginResponseDto loginResponseDto = new LoginResponseDto();
+            loginResponseDto.setJwtToken(jwtToken);
+            loginResponseDto.setJwtExpirationInMilliseconds(jwtService.getExpirationTime());
+
+            Cookie cookie = new Cookie("jwtToken", jwtToken);
+            cookie.setHttpOnly(true);
+            cookie.setSecure(true);
+            cookie.setPath("/");
+            cookie.setMaxAge((int) jwtService.getExpirationTime());
+            httpServletResponse.addCookie(cookie);
+
+            return ResponseEntity.status(HttpStatus.OK).body(loginResponseDto);
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Invalid credentials");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Customer login failed");
+        }
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logoutCustomer(HttpServletResponse response) {
+        Cookie cookie = new Cookie("jwtToken", null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+
+        return ResponseEntity.ok().body("Customer logged out successfully");
+    }
+
+    @GetMapping("/check-session")
+    public ResponseEntity<?> checkCustomerSession(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("jwtToken".equals(cookie.getName()) && jwtService.validateToken(cookie.getValue())) {
+                    return ResponseEntity.ok().body("Session is valid.");
+                }
+            }
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Session is not valid.");
+    }
+}
---
+++ src/main/java/org/example/service/CustomerService.java
@@ -0,0 +1,58 @@
+package org.example.service;
+
+import org.example.dto.CustomerDto;
+import org.example.dto.CustomerLoginDto;
+import org.example.exception.EmailAlreadyInUseException;
+import org.example.model.Customer;
+import org.example.repository.CustomerRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public CustomerService(CustomerRepository customerRepository, AuthenticationManager authenticationManager) {
+        this.customerRepository = customerRepository;
+        this.authenticationManager = authenticationManager;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    @Transactional
+    public Customer registerCustomer(CustomerDto customerDto) {
+        if (customerRepository.findByEmail(customerDto.getEmail()).isPresent()) {
+            throw new EmailAlreadyInUseException("Email already in use");
+        }
+        Customer customer = convertCustomerDtoToEntity(customerDto);
+        return customerRepository.save(customer);
+    }
+
+    public Customer authenticateCustomer(CustomerLoginDto customerLoginDto) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        customerLoginDto.getEmail(),
+                        customerLoginDto.getPassword()
+                )
+        );
+        return customerRepository.findByEmail(customerLoginDto.getEmail()).orElse(null);
+    }
+
+    private Customer convertCustomerDtoToEntity(CustomerDto customerDto) {
+        Customer customer = new Customer();
+        customer.setFirstName(customerDto.getFirstName());
+        customer.setLastName(customerDto.getLastName());
+        customer.setPhoneNumber(customerDto.getPhoneNumber());
+        customer.setLicenceNumber(customerDto.getLicenceNumber());
+        customer.setEmail(customerDto.getEmail());
+        customer.setPassword(this.passwordEncoder.encode(customerDto.getPassword()));
+
+        return customer;
+    }
+}
